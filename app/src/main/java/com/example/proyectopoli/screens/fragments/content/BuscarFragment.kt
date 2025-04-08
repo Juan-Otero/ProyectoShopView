c6677fa4c771c959f7a0ba1443e24665050d2560
Juan David Otero <oteroperezjuandavid1@gmail.com>
2025-04-08 13:35:30 -0500

    (Yaneth Trujillo) Fragment Buscar
    Commit realizado por Yaneth Trujillo. Se sube desde esta cuenta debido a problemas con repositorio local de la compañera.

--- app/src/main/java/com/example/proyectopoli/screens/fragments/content/BuscarFragment.kt
+++ app/src/main/java/com/example/proyectopoli/screens/fragments/content/BuscarFragment.kt
@@ -1,30 +1,104 @@
 package com.example.proyectopoli.screens.fragments.content
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.proyectopoli.ui.theme.BlackButton
 
 @Composable
 @Preview
 fun BuscarFragment() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "Se ha seleccionado la opción Buscar ",
-            style = MaterialTheme.typography.headlineMedium,
-            textAlign = TextAlign.Center
-        )
+    var searchQuery by remember { mutableStateOf("") }
+
+    // Barra superior de fragment Item
+    Scaffold(
+        topBar = { TopBar() }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .background(Color.White)
+        ) {
+            Spacer(modifier = Modifier.height(40.dp))
+
+            // Campo de búsqueda
+            OutlinedTextField(
+                value = searchQuery,
+                onValueChange = { searchQuery = it },
+                placeholder = { Text(
+                    "Buscar producto",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = Color.Gray) },
+
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .fillMaxWidth()
+                    .height(60.dp),
+
+                shape = RoundedCornerShape(40),
+                trailingIcon = {
+                    if (searchQuery.isNotEmpty()) {
+                        IconButton(onClick = { searchQuery = "" }) {
+                            Icon(Icons.Default.Close, contentDescription = "Limpiar búsqueda")
+                        }
+                    }
+                },
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedContainerColor = Color.White,
+                    focusedContainerColor = Color.White
+                )
+            )
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            Text(
+                text = "Resultados para búsqueda",
+                fontSize = 34.sp,
+                fontWeight = FontWeight.Light,
+                color = BlackButton,
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            // Grid de productos
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                verticalArrangement = Arrangement.spacedBy(26.dp)
+            ) {
+
+                // Filtrado para mostrar solo los productos buscados
+                val filteredProducts = getMockFeaturedProducts().filter { product ->
+                    product.name.contains(searchQuery, ignoreCase = true)
+                }
+
+                // Lista de productos de prueba
+                items(filteredProducts) { product ->
+                    ProductCard(product)
+                }
+            }
+        }
     }
 }
